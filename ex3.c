1520f8149edf2e3a25c2ca35d431601762aaceba
NotPirralho <hugomiguel052@gmail.com>
2024-04-08 19:28:07 +0100

    ex3 resolvido

--- ex3.c
+++ ex3.c
@@ -39,7 +39,9 @@ int main()
     scanf("%d%d", &num1, &num2);
     
     // Call maximum function
+    maximum = max(num1, num2);
     // Call minimum function
+    minimum = min(num1, num2);
     
     printf("\nMaximum = %d\n", maximum);
     printf("Minimum = %d", minimum);
@@ -53,7 +55,10 @@ int main()
  */
 int max(int num1, int num2)
 {
-    
+    if(num1 > num2)
+        return num1;
+    else
+        return num2;
 }
 
 /**
@@ -61,5 +66,8 @@ int max(int num1, int num2)
  */
 int min(int num1, int num2) 
 {
-    
+    if(num1 < num2)
+        return num1;
+    else
+        return num2;
 }
